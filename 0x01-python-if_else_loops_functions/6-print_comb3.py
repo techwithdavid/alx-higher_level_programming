4ac5afcbc8927665f0b18e87e77fc210d6888c0a
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-03-07 14:31:06 -0800

    Fix: Removed semicolon on line 9

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for i in range(0, 10):
     for j in range(1, 10):
         if i >= j:
             continue
-        if i==8 and j==9:
+        if i == 8 and j == 9:
             print("{:d}{:d}".format(i, j))
         else:
-            print("{:d}{:d}".format(i, j), end=", ");
+            print("{:d}{:d}".format(i, j), end=", ")
