15ea20825a76105a529909851ce2bba8d24d342e
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-03-07 13:03:43 -0800

    Fix: Fixed another bug

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-for _ in range(1, 100):
-    if _%3==0:
-        print("Fizz", end=" ")
-    elif _%5==0:
-        print("Buzz", end=" ");
-    elif _%3==0 and _%5==0:
-        print("FizzBuzz", end=" ")
-    else:
-        print("{:d}".format(_), end=" ")
+def fizzbuzz():
+    for _ in range(1, 100):
+        if _%3==0:
+            print("Fizz", end=" ")
+        elif _%5==0:
+            print("Buzz", end=" ");
+        elif _%3==0 and _%5==0:
+            print("FizzBuzz", end=" ")
+        else:
+            print("{:d}".format(_), end=" ")
