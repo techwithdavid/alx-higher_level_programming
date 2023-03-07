9fe6f87bd3c97110d3ff79fa62fde8cd7c2f4a06
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-03-07 12:58:26 -0800

    Added the file

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for _ in range(1, 100):
+    if _%3==0:
+        print("Fizz", end=" ")
+    elif _%5==0:
+        print("Buzz", end=" ");
+    elif _%3==0 and _%5==0:
+        print("FizzBuzz", end=" ")
+    else:
+        print("{:d}".format(i), end=" ")
