985256f1122c58f91ebf90b34f74e23241c42a23
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-03-09 21:47:08 -0800

    Fixed a logical error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
         print("{} arguments.".format(length))
 
     for i in range(length):
-        print("{}: {}".format(i, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
