d58e47659d422d384c18fbf7b37d8c10eae242d2
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-03-13 13:43:07 -0700

    Update: Updated the code to return a tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    tupl = ()
     if len(sentence) == 0:
-        return None
-    return (len(sentence), sentence[0])
+        tupl = 0, None
+    else:
+        tupl = len(sentence), sentence[0]
+    return tupl
