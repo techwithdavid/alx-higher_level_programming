f730b37271931561ed464300c87d7ff68aed8227
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-03-13 09:22:57 -0700

    Update: Updated the file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        print(matrix[i])
+    for row in matrix:
+        for col in row:
+            print("{}".format(col), end=" ")
+        print()
+
