d206719392c7a37a279b86a58aeaf4ab9b4111ef
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-03-13 08:07:53 -0700

    Update: Updated the code

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-
+    length = len(my_list)
+    if idx < 0 or idx >= length:
+        return my_list
+    new_list = [0] * length
+    for i in range(0, length):
+        new_list[i] = my_list[i]
+    new_list[idx] = element
+    return new_list
