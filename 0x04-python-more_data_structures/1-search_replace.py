f742f2e890ebd7da5dd82b590429f19a871a09d7
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-03-16 13:18:11 -0700

    Update: Updated the code to follow pycodestyle rules

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,4 +6,4 @@ def search_replace(my_list, search, replace):
             new_list.append(replace)
         else:
             new_list.append(i)
-    return new_list            
+    return new_list
