92ff9f4ddee593f24b97ae773a65d44d2e51f6ea
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-03-16 16:11:43 -0700

    Added the file

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,9 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     max_value = 0
-    for i in a_dictionary.values():
-        if i > max_value:
-            max_value = i
-    return max_value
+    for value in a_dictionary.values():
+        if value > max_value:
+            max_value = value
+    for key, value in a_dictionary.items():
+        if value == max_value:
+            return key
