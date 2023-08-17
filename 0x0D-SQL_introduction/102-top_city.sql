6455963de9181f3ef39ebddf2aadf24af22389e7
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-08-17 12:04:26 -0700

    Updated the file

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Write a script that displays the top 3 of cities temperature
 SELECT city, AVG(value) as avg_temp
 FROM temperatures
-WHERE month BETWEEN 7 AND 8
+WHERE month = 7 OR month = 8
 GROUP BY city
-ORDER BY 2 DESC
+ORDER BY avg_temp DESC
 LIMIT 3;
